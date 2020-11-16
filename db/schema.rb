7026b6f06a2ae2f905a7b7baa4fb7cfd6067055a
baptistedangy <baptiste1.dangy@gmail.com>
2020-11-16 16:21:14 +0100

    Status on booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_142218) do
+ActiveRecord::Schema.define(version: 2020_11_16_152013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_142218) do
     t.bigint "bunker_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["bunker_id"], name: "index_bookings_on_bunker_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
