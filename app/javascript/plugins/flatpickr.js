b94784aaa5d3f73fb0d10b9e74a3d61ff05c9e2d
baptistedangy <baptiste1.dangy@gmail.com>
2020-11-17 18:15:17 +0100

    Search Bar

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,16 @@
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+
+flatpickr(".datepicker", {
+  altInput: true,
+  allowInput: true,
+  enableTime: true
+});
+
+
+// flatpickr(".datepicker" {
+//   altInput: true,
+//   allowInput: true,
+//   enableTime: true
+// });
