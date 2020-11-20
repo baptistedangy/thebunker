3ba756b692853cb7a36d66a29d491c8890ce5106
Bastien Duquesne <bastienmsn@gmail.com>
2020-11-20 09:07:48 +0100

    pending booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @user = current_user
     # Recuperer les bunkers qui appartiennent aux users logged (proprio)
     @ownerbunkers = Bunker.where(user: @user)
-    @bookings = Booking.where(user: @user)
+    # Recuperer les demandes de bookings d'un autre utilisateur
+    @bookings = Booking.where(bunker: @ownerbunkers)
   end
 end
